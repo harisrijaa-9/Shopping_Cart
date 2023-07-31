accabbb610764763f8020c5fef0b77884b683b12
harisrijaa-9 <srijaa.19je0477@mech.iitism.ac.in>
2023-07-31 20:43:18 +0530

    first commit

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
+
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
+  console.log("Printing Cart");
+  console.log(cart);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
+  return (
+    <div>
+      {cart.length > 0 ? (
+        <div>
+          <div>
+            {cart.map((item, index) => {
+              return <CartItem key={item.id} item={item} itemIndex={index} />;
+            })}
+          </div>
+
+          <div>
+            <div>
+              <div className="font-bold  text-blue-600 text-[22px]">
+                Your Cart
+              </div>
+              <div>Summary</div>
+              <p>
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
+
+            <div>
+              <p>Total Amount: ${totalAmount}</p>
+              <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                CheckOut Now
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Cart Empty</h1>
+          <Link to={"/"}>
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
